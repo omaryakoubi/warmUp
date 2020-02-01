b82c0348b2bfaf6120e99e61f50a3004a1a57da5
omaryakoubi <yakoubiomar2001@gmail.com>
2020-02-01 09:58:31 +0100

    Omar Yakoubi warmUp1

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -4,3 +4,33 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+
+//Exercise 1
+var firstName="Omar"
+var lastName="Yakoubi"
+console.log(fistName+" "+lastname)
+
+
+//Exercise 2
+var x=13
+if(x%3=0){
+return x "Is a multiple of" 3}
+return x "Not a multiple if" 3
+
+
+//Exercise 3
+var ages=[13,14,13,15,16,17,19,13,16,15]
+function averageAge(array){
+var average=0
+for(var i=0;i<ages.length;i++){
+	average=(ages[i]/ages.length)+average	
+}
+return (average)
+}
+
+
+//Exercise 4
+function ageInSeconds(age){
+	return (age*365.25*24*60*60)
+} 
