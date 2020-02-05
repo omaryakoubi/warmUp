e8f03bb836f1353099407fcc7ba01f74f1a4d59e
omaryakoubi <yakoubiomar2001@gmail.com>
2020-02-05 10:02:24 +0100

    Omar Yakoubi warUp4

--- warmUp4/warmUp4.js
+++ warmUp4/warmUp4.js
@@ -1,9 +1,28 @@
 // 1-Define a function called myTrueFun that returns true whenever its called.
+function myTrueFun() {
+	return true
+}
 // 2-complete the following color function in which returns true if type of the input is string and returns
 // 	false otherwise.
-
-	function typeFunc(value){
-
+function typeValue(value) {
+	if (typeof (value) === "string") {
+		return true
 	}
+	else { return false }
+}
 // 3-Write a JavaScript function that accepts a number as a parameter and check if the number is prime or not.
-// 4-write a function that accepts two numbers as parameters and returns true if the two parameters have the same value , false if not
+// I think primative numbers is numbers with only one figure i dont know if is right because i'm not allowed to use chrome
+function checkNum(number) {
+	if (number > 10);
+	return "The Number" + " " + number + " " + "is prime"
+}
+// 4-write a function that accepts two numbers as parameters and returns true if the two parameters have the same value , false if not.
+function sameNum(num1, num2) {
+	if (num1 === num2) {
+		return true
+	}
+	else {
+		return false
+	}
+}
+
