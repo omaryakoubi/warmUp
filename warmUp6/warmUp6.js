442c0780a89905ec92fdc0d3148c031aa2a5268f
omaryakoubi <yakoubiomar2001@gmail.com>
2020-02-07 10:01:12 +0100

    Omar Yakoubi warmUp6

--- warmUp6/warmUp6.js
+++ warmUp6/warmUp6.js
@@ -2,19 +2,24 @@
 // objKey({name:'ahmed',age:20}) ==> ['name','age']
 
 function objKey(obj) {
-  // your code here...
+  return (Object.keys(obj))
+
 }
 
 // 2 - Write a function named objLength to get the length of a JavaScript object
 // objLength({name:'ahmed',age:20}) ==> 2
 
 function objLength(obj) {
-  // your code here...
+  return (Object.keys(obj)).length
 }
 
 // 3 - Write a function named objSort to sort an array of JavaScript objects based on the id
 // objSort([{id:2,value:50},{id:0,value:70},{id:1,value:40}]) ==> [{id:0,value:70},{id:1,value:40},{id:2,value:50}]
 
 function objSort(obj) {
-  // your code here...
+  var Array2 = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    Array2.push(obj[i]);
+  }
+  return Array2;
 }
