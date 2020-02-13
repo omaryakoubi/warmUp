4395f35dff42331a2f2a8bb260b69117a44e7e30
omaryakoubi <yakoubiomar2001@gmail.com>
2020-02-13 09:58:12 +0100

    Omar Yakoubi warmUp10

--- warmUp10/warmUp-10.js
+++ warmUp10/warmUp-10.js
@@ -2,6 +2,18 @@
 //followed by a number starting from zero(solve it using while loop).
 // example:    reverseStr('hello'); "o1l2l3e4h"
 
+function reverseString(str) {
+    var i = str.length
+    while (i > 0)
 
-// 2-write a function that takes array of strings and returns an array of the strings that have the same length
-// example :  ['hi','hello','welcome','hy'] == > ["hi","hy"]
+        // 2-write a function that takes array of strings and returns an array of the strings that have the same length
+        // example :  ['hi','hello','welcome','hy'] == > ["hi","hy"]
+
+        function sameLength(array) {
+            array2 = []
+            for (i = 0; array.length > i; i++) {
+                array[i] === array[i]
+                array2.push(array[i])
+            }
+        } return array2
+}
